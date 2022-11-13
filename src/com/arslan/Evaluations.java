78d10fe900acbc564bdb3b1aa257e7b96b4396e1
Arslan Rasheed <arslanrashee1@gmail.com>
2022-11-13 07:50:36 +0500

    making evaluation class

--- src/com/arslan/Evaluations.java
+++ src/com/arslan/Evaluations.java
@@ -1,4 +1,17 @@
 package com.arslan;
 
 public class Evaluations {
+    private  int evaluationId;
+
+    Evaluations(int evaluationId){
+        this.setEvaluationId(evaluationId);
+    }
+
+    private void setEvaluationId(int evaluationId) {
+        this.evaluationId = evaluationId;
+    }
+
+    public int getEvaluationId() {
+        return evaluationId;
+    }
 }
