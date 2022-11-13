74ffb153efaa0295243eee56a803c47efd33b72f
Arslan Rasheed <arslanrashee1@gmail.com>
2022-11-13 08:01:19 +0500

    start of evalution class

--- src/com/arslan/Evaluations.java
+++ src/com/arslan/Evaluations.java
@@ -1,17 +1,4 @@
 package com.arslan;
 
 public class Evaluations {
-    private  int evaluationId;
-
-    Evaluations(int evaluationId){
-        this.setEvaluationId(evaluationId);
-    }
-
-    private void setEvaluationId(int evaluationId) {
-        this.evaluationId = evaluationId;
-    }
-
-    public int getEvaluationId() {
-        return evaluationId;
-    }
 }
